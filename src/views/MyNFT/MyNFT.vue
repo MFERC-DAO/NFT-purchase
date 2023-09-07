084b2dbde43ebd67a93a536318197f6ff3bcfabf
lingko_33 <lingko0246@163.com>
2023-09-07 11:04:37 +0800

    页面完成

--- src/views/MyNFT/MyNFT.vue
+++ src/views/MyNFT/MyNFT.vue
@@ -1,38 +1,47 @@
 <template>
   <div class="myNFT">
-    <!-- <Header></Header> -->
+    <Header></Header>
     <article>
       <div class="title-cont">
         <h1>我的</h1>
         <h5>0X1F..8963</h5>
         <div class="line"></div>
       </div>
-      <h3 class="sub-title">我的黑色蜜蜂</h3>
-      <div>
+      <div class="cont">
+        <h3 class="sub-title">我的黑色蜜蜂</h3>
         <ul class="fx fx-wrap">
           <li v-for="item in 2">
-            <div class="cover"></div>
-            <div class="img-cont"><img src="@/assets/img/bee_img.jpg" alt=""></div>
+            <div class="bee-cont">
+              <div class="cover"></div>
+              <div class="img-cont"><img src="@/assets/img/bee_img.jpg" alt=""></div>
+            </div>
+            <h3>#B009</h3>
           </li>
         </ul>
       </div>
       <div class="line"></div>
-      <h3 class="sub-title">我的紫色蜜蜂</h3>
-      <div>
+      <div class="cont">
+        <h3 class="sub-title">我的紫色蜜蜂</h3>
         <ul class="fx fx-wrap">
           <li v-for="item in 2">
-            <div class="cover"></div>
-            <div class="img-cont"><img src="@/assets/img/bee_img.jpg" alt=""></div>
+            <div class="bee-cont">
+              <div class="cover"></div>
+              <div class="img-cont"><img src="@/assets/img/bee_img.jpg" alt=""></div>
+            </div>
+            <h3>#P045</h3>
           </li>
         </ul>
       </div>
       <div class="line"></div>
-      <h3 class="sub-title">我的金色蜜蜂</h3>
-      <div>
+      <div class="cont-g">
+        <h3 class="sub-title">我的金色蜜蜂</h3>
         <ul class="fx fx-wrap">
           <li v-for="item in 20">
-            <div class="cover"></div>
-            <div class="img-cont"><img src="@/assets/img/bee_img.jpg" alt=""></div>
+            <div class="bee-cont">
+              <div class="cover"></div>
+              <div class="img-cont"><img src="@/assets/img/bee_img.jpg" alt=""></div>
+            </div>
+            <h3>#G0015</h3>
           </li>
         </ul>
       </div>
