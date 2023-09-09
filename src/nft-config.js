de162ccd1c5cd35c3a31a85762a5a8f365ab2bf5
terry3t <wangxi8206@sina.com>
2023-09-09 21:19:18 +0800

    add golden bee base url

--- src/nft-config.js
+++ src/nft-config.js
@@ -7,22 +7,16 @@ export const BeeContracts = {
 
 export const BaseUrl = {
   'black': "",
-  'golden': "",
+  'golden': "https://gateway.nutbox.app/ipns/k51qzi5uqu5dil44fz8dwqh84boop5moe5qojmifrx2036p7vloi2wmum7y68w/",
   'purple': ""
 }
 
 export const BlackBeeCANBaseUri = '';
 
 export const Description = {
-    'black': `MCO（Mferc Community OG）is a functional NFT based on the ERC-721 protocol issued by Mferc Community . The total number of NFTs for sale is 66. NFT holders will have the corresponding features and benefits with Mferc Community. 
-    Whitelisting is required to mint. 
-    Category PFPs. `,
-    'purple': `MCB（Mferc Community Builder）is a functional NFT based on the ERC-721 protocol issued by Mferc Community . The total number of NFTs for sale is 666. NFT holders will have the corresponding features and benefits with Mferc Community. 
-    Whitelisting is required to mint. 
-    Category PFPs. `,
-    'golden': `MCM（Mferc Community Member）is a functional NFT based on the ERC-721 protocol issued by Mferc Community . The total number of NFTs for sale is 6666. NFT holders will have the corresponding features and benefits with Mferc Community. 
-    No whitelist required, anyone can mint directly. 
-    Category PFPs.`
+    'black': `MCO(Mferc Community OG) is a functional NFT based on the ERC-721 protocol issued by Mferc Community.\n The total number of NFTs for sale is 66.\nNFT holders will have the corresponding features and benefits with Mferc Community.\nWhitelisting is required to mint.\nCategory PFPs. `,
+    'purple': `MCB(Mferc Community Builder) is a functional NFT based on the ERC-721 protocol issued by Mferc Community.\n The total number of NFTs for sale is 666.\nNFT holders will have the corresponding features and benefits with Mferc Community.\nWhitelisting is required to mint.\nCategory PFPs. `,
+    'golden': `MCM(Mferc Community Member) is a functional NFT based on the ERC-721 protocol issued by Mferc Community.\n The total number of NFTs for sale is 6666.\nNFT holders will have the corresponding features and benefits with Mferc Community.\nNo whitelist required, anyone can mint directly.\nCategory PFPs.`
   }
 
 export const BlackUris = [];
